e7a62b30c9ae1bff183f891a051ecad16b815cdf
shaun <shaunboo.22@ichat.sp.edu.sg>
2024-01-21 23:16:48 +0800

    kani dont kill me i do tmr the resizing

--- contactus.php
+++ contactus.php
@@ -8,7 +8,9 @@
     <link href="common.css" rel="stylesheet" type="text/css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link href="contactus.php" rel="stylesheet">
 </head>
+
 <body>
 <button class="btn btn-primary d-md-none" type="button" onclick="toggleSidebar()">
     <i class="fas fa-bars"></i>
@@ -69,47 +71,39 @@
         </nav>
 
         <!-- Main Content Area -->
-        <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-            <section id="seasonals">
-                <h2 style="margin-top: 27px;">Contact Us</h2>
-
-                <table style="margin-left: auto; margin-right:auto">
-                    <tr>
-                        <td><img src="assets/logo.png" alt="Delicious Food Logo" style="padding-right: 50px; scale: 80%"></td>
-                        <td style="font-size: x-large">
-                            <a href="mailto:deliciousfood@contactus.org" style="color: #B0B0B0">DeliciousFood@contactus.org</a>
-                            <br><br>
-                            Dr Ong Shern Wen Benjamin <br>
-                            CEO
-                            <br><br>
-                            +65 8246 8078 <br> <br>
-                            <a href="https://www.facebook.com"><img src="assets/facebook.png" alt="Facebook" style="height: 29px; scale: 1.1"></a>
-                            <a href="https://www.instagram.com"><img src="assets/instagram.webp" alt="Instagram" style="height: 30px"></a>
-                            <a href="https://www.twitter.com"><img src="assets/X_logo.png" alt="X" style="height: 29px"></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <hr>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="assets/restaurant.png" alt="Restaurant 1">
-                            <br><br>
-                            BenDover Road 7 <br>
-                            177269 <br><br>
-                            +65 9027 7661
-                        </td>
-                        <td>
-                            <img src="assets/restaurant2.png" alt="Restaurant 2">
-                            <br><br>
-                            MBS <br>
-                            182991 <br><br>
-                            +65 8773 4691
-                        </td>
-                    </tr>
-                </table>
+        <main class="col-12 col-md-9 col-lg-10 px-md-4 scrollable-section shift-down">
+            <section id="contact" class="pt-5">
+                <div class="container">
+                    <div class="text-center mb-5">
+                        <h2>Contact Us</h2>
+                        <p>Get in touch with us for any queries or reservations.</p>
+                        <img src="assets/logo.png" alt="Delicious Food Logo" class="logo mb-3">
+                        <h3>Delicious Food</h3>
+                        <p>Dr Ong Shern Wen Benjamin<br>CEO</p>
+                        <p>+65 8246 8078</p>
+                        <p><a href="mailto:deliciousfood@contactus.org">DeliciousFood@contactus.org</a></p>
+                        <div class="social-icons">
+                            <a href="https://www.facebook.com" class="mr-2"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://www.instagram.com" class="mr-2"><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a>
+                        </div>
+                    </div>
+                    <h2 class="text-center mb-4">Locations</h2>
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 mb-4">
+                            <div class="contact-detail text-center">
+                                <img src="assets/restaurant.png" alt="Restaurant 1" class="img-fluid mb-2">
+                                <p>BenDover Road 7, 177269<br>+65 9027 7661</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="contact-detail text-center">
+                                <img src="assets/restaurant2.png" alt="Restaurant 2" class="img-fluid mb-2">
+                                <p>MBS, 182991<br>+65 8773 4691</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     </div>
