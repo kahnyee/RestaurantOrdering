d21bb580d159ee98953d0c1aa7a5874e85f983fd
kahnyee <kahnyee.22@ichat.sp.edu.sg>
2024-01-21 15:44:26 +0800

    Try

--- aboutus.php
+++ aboutus.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-?>
-=======
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>About Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="common.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<h1>Hello, world!</h1>
+<h1>About Us</h1>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
->>>>>>> 5340fddcb2cace362fd14369e0e8badb67d6e467
--- start.php
+++ start.php
@@ -1,13 +1,23 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Start Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="common.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<h1>Hello, world!</h1>
+<!-- Centered bubbles using Bootstrap classes -->
+<div class="container align-items-center">
+    <div class="rounded-4 p-4 text-center text-white" style="background-color: #B4B4B4;">
+        <h2>Table No: 69</h2>
+    </div>
+</div>
+
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <a href="menu.php" class="btn" style="background-color: #B4B4B4;">Click here to start</a>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
